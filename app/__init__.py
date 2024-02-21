a9dc785591d9a8f523d32cd09db0ac6805f612d1
Binary-Phantom <profissional.viniciusproenca@gmail.com>
2024-02-21 20:53:42 -0300

    feat: db

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlachemy import SQLAlchemy
+
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy (app)
 from controller import default
