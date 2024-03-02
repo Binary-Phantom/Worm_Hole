21a31b5ad2665afa5b121b4e87f03ce355f2d38d
Binary-Phantom <profissional.viniciusproenca@gmail.com>
2024-03-02 00:15:51 -0300

    feat: cadastro usuario

---
+++ minhocario/admin/forms.py
@@ -0,0 +1,12 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+
+class RegistrationForm(Form):
+    username = StringField('Nome', [validators.Length(min=4, max=25)])
+    nickname = StringField('Usuario', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=35),])
+    password = PasswordField('Crie sua senha', [
+        validators.DataRequired(),
+        validators.EqualTo('confirmar senha', message='As senhas devem ser iguais')
+    ])
+    confirm = PasswordField('Digite sua senha novamente')
+    
--- minhocario/admin/routes.py
+++ minhocario/admin/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, session, request, url_for
+from flask import render_template, session, request, url_for, flash, redirect
 from minhocario import app, db
+from .forms import RegistrationForm
 
 @app.route('/')
 
@@ -7,7 +8,13 @@ def home():
     return "Seja bem vindo ao minhocário"
 
 
-@app.route('/registrar')
-
+@app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
-    return render_template('admin/registrar.html', title="Cadastrar Usuário")
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        #user = User(form.username.data, form.email.data,
+              #      form.password.data)
+       # db_session.add(user)
+        flash('Obrigado por se cadastrar')
+        return redirect(url_for('login'))
+    return render_template('admin/registrar.html', form=form, title="Cadastrar novo usuário")
